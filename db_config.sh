28fd7dc2206928359404c1713109226c8af2957b
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-11-23 20:11:34 -0800

    Formatting

--- db_config.sh
+++ db_config.sh
@@ -1,6 +1,6 @@
 apt-get -qqy update
-apt-get -qqy install postgresql python-psycopg2
 
+apt-get -qqy install postgresql python-psycopg2
 su postgres -c 'createuser -dRS vagrant'
 su postgres -c "psql -c \"CREATE USER webapp with password 'secretwebapppassword';\""
 su vagrant -c 'createdb'
--- web_config.sh
+++ web_config.sh
@@ -1,7 +1,11 @@
 apt-get -qqy update
+
 apt-get -qqy install apache2
 apt-get -qqy install libapache2-mod-wsgi
+
 apt-get -qqy install postgresql python-psycopg2
+su postgres -c 'createuser -dRS vagrant'
+
 apt-get -qqy install python-pip
 
 echo 'cd /vagrant' > /home/vagrant/.bash_profile
