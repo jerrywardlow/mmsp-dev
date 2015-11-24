421a661423941e56c574eb1b9cedc24a48227a3a
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-11-23 20:03:37 -0800

    Install Postgres and psycopg2

--- web_config.sh
+++ web_config.sh
@@ -1,6 +1,7 @@
 apt-get -qqy update
 apt-get -qqy install apache2
 apt-get -qqy install libapache2-mod-wsgi
+apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-pip
 
 echo 'cd /vagrant' > /home/vagrant/.bash_profile
