8d7f430a5f1ca0dd7cc73c98a5a75861aaca2f1f
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-11-23 19:53:29 -0800

    Create Postgres DB and users

--- db_config.sh
+++ db_config.sh
@@ -1,4 +1,9 @@
 apt-get -qqy update
 apt-get -qqy install postgresql python-psycopg2
 
+su postgres -c 'createuser -dRS vagrant'
+su postgres -c "psql -c \"CREATE USER webapp with password 'secretwebapppassword';\""
+su vagrant -c 'createdb'
+su vagrant -c 'createdb webappdb'
+
 echo 'cd /vagrant' > /home/vagrant/.bash_profile
