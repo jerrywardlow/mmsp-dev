0b16695366db710ba08abaa4fce1fe8ed4fce278
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-11-23 21:57:01 -0800

    Removed creation of postgres user 'vagrant'

--- web_config.sh
+++ web_config.sh
@@ -4,7 +4,6 @@ apt-get -qqy install apache2
 apt-get -qqy install libapache2-mod-wsgi
 
 apt-get -qqy install postgresql python-psycopg2
-su postgres -c 'createuser -dRS vagrant'
 
 apt-get -qqy install python-pip
 
