623fa0203caaf949b9c7f40ea886d743cd5f2012
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-11-23 20:36:18 -0800

    Added comments for clarity

--- db_config.sh
+++ db_config.sh
@@ -1,7 +1,10 @@
 apt-get -qqy update
 
+# Install Postgres and psycopg2
 apt-get -qqy install postgresql python-psycopg2
+# Create new posgres user 'vagrant'
 su postgres -c 'createuser -dRS vagrant'
+# Create user 'webapp' with pasword
 su postgres -c "psql -c \"CREATE USER webapp with password 'secretwebapppassword';\""
 su vagrant -c 'createdb'
 su vagrant -c 'createdb webappdb'
