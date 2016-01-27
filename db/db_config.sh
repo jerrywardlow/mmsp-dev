77dcdab04e9aa46f47a176f57a830d9fbc412b00
Jerry Wardlow <jerrylikesbikes@gmail.com>
2016-01-26 21:08:59 -0800

    Fixed typos

--- db/db_config.sh
+++ db/db_config.sh
@@ -2,9 +2,9 @@ apt-get -qqy update
 
 # Install Postgres and psycopg2
 apt-get -qqy install postgresql python-psycopg2
-# Create new posgres user 'vagrant'
+# Create new Postgres user 'vagrant'
 su postgres -c 'createuser -dRS vagrant'
-# Create user 'webapp' with pasword
+# Create user 'webapp' with password
 # su postgres -c "psql -c \"CREATE USER webapp with password 'secretwebapppassword';\""
 su vagrant -c 'createdb'
 su vagrant -c 'createdb webappdb'
